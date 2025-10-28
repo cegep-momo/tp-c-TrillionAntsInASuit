2f9b7eab84ea363d05a0bbc9383eb9283204a2ce
Raffi Boyajian <2279458@cmontmorency.qc.ca>
2025-10-28 00:08:50 +0000

    Utiliser Cmake pour tester fonctionalites.

---
+++ libBibliotheque/build/CMakeFiles/bibliotheque.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/book.cpp" "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/build/CMakeFiles/bibliotheque.dir/book.cpp.o"
+  "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/filemanager.cpp" "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/build/CMakeFiles/bibliotheque.dir/filemanager.cpp.o"
+  "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/library.cpp" "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/build/CMakeFiles/bibliotheque.dir/library.cpp.o"
+  "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/main.cpp" "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/build/CMakeFiles/bibliotheque.dir/main.cpp.o"
+  "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/user.cpp" "/workspaces/tp-c-TrillionAntsInASuit/libBibliotheque/build/CMakeFiles/bibliotheque.dir/user.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ libBibliotheque/build/CMakeFiles/bibliotheque.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bibliotheque.dir/book.cpp.o"
+  "CMakeFiles/bibliotheque.dir/filemanager.cpp.o"
+  "CMakeFiles/bibliotheque.dir/library.cpp.o"
+  "CMakeFiles/bibliotheque.dir/main.cpp.o"
+  "CMakeFiles/bibliotheque.dir/user.cpp.o"
+  "bibliotheque"
+  "bibliotheque.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bibliotheque.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
